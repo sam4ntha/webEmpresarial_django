a5680a4185902e4db623cc872f82553e4786f585
sam4ntha <stap0428@gmail.com>
2023-06-02 01:27:56 -0600

    Primera app Core

---
+++ core/urls.py
@@ -0,0 +1,13 @@
+#Importar urls de forma práctica, extendiendose de forma más cómoda las apps
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('store/', views.store, name='store'),
+    path('contact/', views.contact, name='contact'),
+    path('blog/', views.blog, name='blog'),
+    path('sample/', views.sample, name='sample'),
+]
